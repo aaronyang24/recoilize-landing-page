d6acdb8a26a15a10ca42ea582fac6b25f27cfeb0
davidemmolino <davidewebdev@gmail.com>
2020-10-07 19:01:56 -0700

    adding svg files before installing more modules

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,15 +4,15 @@ import '../index.css';
 export default function Navbar() {
     return (  
         <div>  
-            <ul>
-                <li><a href="https://www.google.com">Team</a></li>
-                <li><a href="https://www.google.com">Playground</a></li>
-            </ul>
-            <ul>
-                {/* fix the links, these should go to the corresponding social media site */}
-                <li><a href="https://www.google.com"><img src="" alt="links to linkedin"/></a></li>
-                <li><a href="https://www.google.com"><img src="" alt="linked to github"/></a></li>
-            </ul>
+                <ul>
+                    <li><a href="https://www.google.com">Team</a></li>
+                    <li><a href="https://www.google.com">Playground</a></li>
+                </ul>
+                <ul>
+                    {/* fix the links, these should go to the corresponding social media site */}
+                    <li><a href="https://www.google.com"><img src={} alt="links to linkedin"/></a></li>
+                    <li><a href="https://www.google.com"><img src={} alt="linked to github"/></a></li>
+                </ul>
         </div>
     )
 }
