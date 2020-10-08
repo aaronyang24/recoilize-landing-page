c8dc4dd77a0c014ee0a62275d38b0c3af2ad6480
davidemmolino <davidewebdev@gmail.com>
2020-10-07 20:47:11 -0700

    added svg icons and more styling to landing page and navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
 import '../index.css';
+import React from 'react';
+import Github from '../assets/github-logo.svg';
+import Linkedin from '../assets/linkedin-logo.svg';
+// import { FaGithub } from 'react-icons/fa';
+// import { FaLinkedin } from 'react-icons/fa';
+// {`${FaLinkedin}`}
+// {`${FaGithub}`}
 
 export default function Navbar() {
     return (  
@@ -10,8 +16,8 @@ export default function Navbar() {
                 </ul>
                 <ul>
                     {/* fix the links, these should go to the corresponding social media site */}
-                    <li><a href="https://www.google.com"><img src={} alt="links to linkedin"/></a></li>
-                    <li><a href="https://www.google.com"><img src={} alt="linked to github"/></a></li>
+                    <li><a href="https://www.google.com"><img id="linkedin-logo" src={Linkedin}alt="link to linkedin"/></a></li>
+                    <li><a href="https://www.google.com"><img id="github-logo" src={Github} alt="link to github"/></a></li>
                 </ul>
         </div>
     )
